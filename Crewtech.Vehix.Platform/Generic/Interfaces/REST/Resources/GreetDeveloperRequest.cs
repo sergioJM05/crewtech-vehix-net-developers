e356637de00ba9ac136a5f37cc168828f855f9c5
GabrielaShapiama28 <u202319448@upc.edu.pe>
2025-04-12 11:59:52 -0500

    feat(greetings): add greet developer request and response

---
+++ Crewtech.Vehix.Platform/Generic/Interfaces/REST/Resources/GreetDeveloperRequest.cs
@@ -0,0 +1,11 @@
+namespace Crewtech.Vehix.Platform.Generic.Interfaces.REST.Resources;
+
+/// <summary>
+/// This is a request to greet a developer.
+/// Contains the first and last name of the developer, used as input for the greeting request.
+/// </summary>
+/// <param name="FirstName">The first name of the developer. Can be null</param>
+/// <param name="LastName">The first name of the developer. Can be null</param>
+public record GreetDeveloperRequest(string? FirstName, string? LastName)
+{
+}
---
+++ Crewtech.Vehix.Platform/Generic/Interfaces/REST/Resources/GreetDeveloperResponse.cs
@@ -0,0 +1,19 @@
+namespace Crewtech.Vehix.Platform.Generic.Interfaces.REST.Resources;
+/// <summary>
+/// This is a response to greet a developer. It contains developer's ID, full name and a message
+/// It is used as output for the greeting request.
+/// </summary>
+/// <param name="Id">The ID of the developer. Can be null</param>
+/// <param name="FullName">The full name of the developer. Can be null</param>
+/// <param name="Message">The greeting message. This is a required field.</param>
+public record GreetDeveloperResponse(Guid? Id, string? FullName, string Message)
+{
+    /// <summary>
+    /// This costructor is used to create a GreetDeveloperResponse with only the message.
+    /// </summary>
+    /// <param name="message">The greeeting message. This is a required field.</param>
+    public GreetDeveloperResponse(string Message) : this(null, null, Message)
+    {
+        
+    }
+}
